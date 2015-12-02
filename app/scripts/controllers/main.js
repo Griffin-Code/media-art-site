7b608ee78ffa1e679368e9305433f5b4bba1a08d
Wehttam664 <matt@vevox.io>
2015-12-02 13:23:57 -0800

    yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mediaArtProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mediaArtProjectApp
+ */
+angular.module('mediaArtProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
