aaa61388c94213ad186be0cb8794aee8120a97fe
Wehttam664 <matt@vevox.io>
2015-12-10 13:27:45 -0800

    Added more content

--- app/scripts/controllers/works.js
+++ app/scripts/controllers/works.js
@@ -8,10 +8,28 @@
  * Controller of the mediaArtProjectApp
  */
 angular.module('mediaArtProjectApp')
-  .controller('WorksCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('WorksCtrl', function ($scope) {
+    $scope.studentWorks = [
+      {
+        title: 'Sample Work 1',
+        desc: 'This is the first student work placeholder',
+        project: 'Project Name',
+        author: 'Student Name',
+        img: 'http://placehold.it/300x200'
+      },
+      {
+        title: 'Sample Work 2',
+        desc: 'This is the next student work placeholder',
+        project: 'Project Name',
+        author: 'Student Name',
+        img: 'http://placehold.it/300x200'
+      },
+      {
+        title: 'Sample Work 3',
+        desc: 'This is the third student work placeholder',
+        project: 'Project Name',
+        author: 'Student Name',
+        img: 'http://placehold.it/300x200'
+      }
     ];
   });
